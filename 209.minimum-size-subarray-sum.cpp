43a582d532f624323f2f432ff8e5e79c86dfb0e4
zwyyy456 <zwyyy456@hotmail.com>
2022-03-25 16:05:40 +0800

    wsl

--- 209.minimum-size-subarray-sum.cpp
+++ 209.minimum-size-subarray-sum.cpp
@@ -5,10 +5,27 @@
  */
 
 // @lc code=start
+#include <vector>
+#include <iostream>
+using std::vector;
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        
+        vector<int> subA;
+        int left = 0;
+        int right = 1;
+        int len = 1;
+        int sum = 0;
+        for (len = 0; len <= nums.size(); len++) {
+            for (left = 0, right = 0 + len; sum < target; left++, right++) {
+                sum = 0;
+                for (int i = left; i <= right; i++)
+                    sum += nums[i];
+            }
+            return len;
+        }
+        return 0;
+
     }
 };
 // @lc code=end

31587308f30d0579936ba40d123970f5dc1b044e
zwyyy456 <zwyyy456@163.com>
2022-08-08 11:48:52 +0800

    lt

--- test.cpp
+++ test.cpp
@@ -1,29 +1,51 @@
 #include <iostream>
 #include <unordered_set>
+#include <vector>
 using std::unordered_set;
-int mysum(int n) {
-    int sum = 0;
-    while (n != 0) {
-        sum += (n % 10) * (n % 10);
-        n = n / 10;
+using std::vector;
+vector<vector<int>> threeSum(vector<int> &nums);
+int main() {
+    vector<int> nums;
+    nums = {-1, 0, 1, 2, -1, -4};
+    auto res = threeSum(nums);
+    for (auto i : res) {
+        for (auto j : i)
+            std::cout << j << " ";
+        std::cout << "\n";
     }
-    return sum;
 }
-bool isHappy(int n) {
-    unordered_set<int> happy;
-    int sum = mysum(n);
-    while (sum != 1) {
-        if (happy.find(sum) != happy.end()) {
-            return false;
-        } else {
-            happy.insert(sum);
-            sum = mysum(sum);
+vector<vector<int>> threeSum(vector<int> &nums) {
+    vector<vector<int>> res;
+    sort(nums.begin(), nums.end());
+    int i = 1, j = nums.size() - 1;
+    if (nums[0] > 0 || nums[nums.size() - 1] < 0)
+        return res;
+    for (int k = 0; k < nums.size() - 2; k++) {
+        if (k > 0 && nums[k] == nums[k - 1])
+            continue;
+        i = k + 1;
+        j = nums.size() - 1;
+        while (i < j) {
+            if (nums[k] + nums[i] + nums[j] > 0) {
+                j--;
+                while (nums[j + 1] == nums[j] && j > i + 1)
+                    j--;
+            } else if (nums[k] + nums[i] + nums[j] < 0 && i < j - 1)
+        }
+        while (nums[k] + nums[i] + nums[j] > 0 && i < j - 1) {
+            j--;
+            while (nums[j + 1] == nums[j] && j > i + 1)
+                j--;
+        }
+        while (nums[k] + nums[i] + nums[j] < 0 && i < j - 1) {
+            i++;
+            while (nums[i - 1] == nums[i] && i < j - 1)
+                i++;
+        }
+        if (nums[k] + nums[i] + nums[j] == 0) {
+            std::cout << "k = " << k << " i = " << i << " j = " << j << "\n";
+            res.push_back({nums[k], nums[i], nums[j]});
         }
     }
-    return true;
-}
-
-int main() {
-    int n = isHappy(19);
-    std::cout << isHappy(19) << "\n";
+    return res;
 }

ae82fb84cdae174d50dfe7769b59fac541b19c1b
zwyyy456 <zwyyy456@hotmail.com>
2022-09-15 10:17:35 +0800

    wsl

--- 17.letter-combinations-of-a-phone-number.cpp
+++ 17.letter-combinations-of-a-phone-number.cpp
@@ -16,13 +16,13 @@ class Solution {
     string letter_map[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     // n为digits的长度
     void track_back(string digits, int index, int n) {
-        if (path.size() == n) {
+        if (path.size() >= n) {
             res.push_back(path);
             return;
         }
-
-        for (char c : letter_map[digits[index] - '0']) {
-            path.push_back(c);
+        string letters = letter_map[digits[index] - '0'];
+        for (int i = 0; i < letters.size(); i++) {
+            path.push_back(letters[i]);
             track_back(digits, index + 1, n);
             path.pop_back();
         }
@@ -33,6 +33,8 @@ class Solution {
   public:
     vector<string> letterCombinations(string digits) {
         track_back(digits, 0, digits.size());
+        if (res.size() == 1 && res[0] == "")
+            res.clear();
         return res;
     }
 };

c7fe0e2492311407ec6812f8a945771c36428b5b
zwyyy456 <zwyyy456@hotmail.com>
2022-09-17 16:28:33 +0800

    mac

---
+++ 77.combinations.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=77 lang=cpp
+ *
+ * [77] Combinations
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        
+    }
+};
+// @lc code=end
+

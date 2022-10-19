0b5b94ec53184dcf17414c02b01afcea6b6eaa46
zwyyy456 <zwyyy456@hotmail.com>
2022-10-19 19:30:21 +0800

    mac

---
+++ 1035.uncrossed-lines.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1035 lang=cpp
+ *
+ * [1035] Uncrossed Lines
+ */
+
+// @lc code=start
+#include <vector>
+using std::vector;
+class Solution {
+  public:
+    int maxUncrossedLines(vector<int> &nums1, vector<int> &nums2) {
+        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
+        for (int i = 1; i <= nums1.size(); i++) {
+            for (int j = 1; j <= nums2.size(); j++) {
+                if (nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = max(max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1] + 1);
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[nums1.size()][nums2.size()];
+    }
+};
+// @lc code=end

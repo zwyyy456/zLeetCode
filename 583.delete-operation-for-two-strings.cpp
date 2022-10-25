a08f5675d9f42279f89643c9fb5a99e2c5ffe9f2
zwyyy456 <zwyyy456@hotmail.com>
2022-10-25 09:23:08 +0800

    mac

---
+++ 583.delete-operation-for-two-strings.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=583 lang=cpp
+ *
+ * [583] Delete Operation for Two Strings
+ */
+
+// @lc code=start
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+class Solution {
+  public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+        for (int i = 0; i <= word1.size(); i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= word2.size(); j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= word1.size(); i++) {
+            for (int j = 1; j <= word2.size(); j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1;
+            }
+        }
+        return dp[word1.size()][word2.size()];
+    }
+};
+// @lc code=end
---
+++ 647.palindromic-substrings.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=647 lang=cpp
+ *
+ * [647] Palindromic Substrings
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countSubstrings(string s) {
+        
+    }
+};
+// @lc code=end
+

d3fc7b530c3e1733de8533dc6c6ef76065960e45
zwyyy456 <zwyyy456@hotmail.com>
2022-11-17 13:38:40 +0800

    change miniconda

--- 84.largest-rectangle-in-histogram.cpp
+++ 84.largest-rectangle-in-histogram.cpp
@@ -17,28 +17,18 @@ class Solution {
         heights.push_back(0);               // 数组尾部加入元素0
         st.push(0);
         int result = 0;
-        // 第一个元素已经入栈，从下标1开始
         for (int i = 1; i < heights.size(); i++) {
-            // 注意heights[i] 是和heights[st.top()] 比较 ，st.top()是下标
-            if (heights[i] > heights[st.top()]) {
-                st.push(i);
-            } else if (heights[i] == heights[st.top()]) {
-                st.pop(); // 这个可以加，可以不加，效果一样，思路不同
-                st.push(i);
-            } else {
-                while (heights[i] < heights[st.top()]) { // 注意是while
-                    int mid = st.top();
-                    st.pop();
-                    int left = st.top();
-                    int right = i;
-                    int w = right - left - 1;
-                    int h = heights[mid];
-                    result = max(result, w * h);
-                }
-                st.push(i);
+            while (heights[i] < heights[st.top()]) {
+                int mid = st.top();
+                st.pop();
+                int w = i - st.top() - 1;
+                int h = heights[mid];
+                result = max(result, w * h);
             }
+            st.push(i);
         }
         return result;
     }
 };
+
 // @lc code=end

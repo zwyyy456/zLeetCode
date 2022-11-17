9cdab75a3d62cf62d873e79ea1fcee4326f3c8b4
zwyyy456 <zwyyy456@hotmail.com>
2022-11-17 18:53:57 +0800

    mac

--- 42.trapping-rain-water.cpp
+++ 42.trapping-rain-water.cpp
@@ -12,33 +12,22 @@ using std::vector;
 class Solution {
   public:
     int trap(vector<int> &height) {
-        if (height.size() <= 2)
-            return 0;  // 可以不加
-        stack<int> st; // 存着下标，计算的时候用下标对应的柱子高度
-        st.push(0);
-        int sum = 0;
+        stack<int> stk;
+        stk.push(0);
+        int res = 0;
         for (int i = 1; i < height.size(); i++) {
-            if (height[i] < height[st.top()]) { // 情况一
-                st.push(i);
-            }
-            if (height[i] == height[st.top()]) { // 情况二
-                st.pop();                        // 其实这一句可以不加，效果是一样的，但处理相同的情况的思路却变了。
-                st.push(i);
-            } else {                                                  // 情况三
-                while (!st.empty() && height[i] > height[st.top()]) { // 注意这里是while
-                    int mid = st.top();
-                    st.pop();
-                    if (!st.empty()) {
-                        int h = min(height[st.top()], height[i]) - height[mid];
-                        int w = i - st.top() - 1; // 注意减一，只求中间宽度
-                        sum += h * w;
-                    }
+            while (!stk.empty() && height[i] > height[stk.top()]) {
+                int mid = stk.top();
+                stk.pop();
+                if (!stk.empty()) {
+                    int h = min(height[i], height[stk.top()]) - height[mid];
+                    int w = i - stk.top() - 1;
+                    res += h * w;
                 }
-                st.push(i);
             }
+            stk.push(i);
         }
-        return sum;
+        return res;
     }
 };
-
 // @lc code=end

d9e32ec0fce47db955fbfbe1f75cdf97e804f85f
zwyyy456 <zwyyy456@hotmail.com>
2023-06-04 17:21:12 +0800

    win11

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zwyyy/OneDrive/Documents/code/zLeetCode
-# Build directory: C:/Users/zwyyy/OneDrive/Documents/code/zLeetCode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

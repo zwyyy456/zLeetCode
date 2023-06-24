6ff4c36e938009a38c500f70a2e5d9e7a691763f
zwyyy456 <zwyyy456@hotmail.com>
2023-06-24 12:49:35 +0800

    wsl, move file

--- rename.py
+++ rename.py
@@ -1,25 +1,16 @@
 import os
-import shutil
-import re
 
-# 在当前目录下查找文件
-for filename in os.listdir("."):
-    # 检查文件名是否以数字和"."开头，并以".cpp"结尾
-    if re.match(r"^[0-9]+\..*\.cpp$", filename):
-        # 获取文件名的前缀（去掉.cpp后缀）
-        prefix = filename.rsplit(".", 1)[0]
-        # 获取数字部分
-        num = prefix.split(".", 1)[0]
-        # 获取后缀部分（去掉数字和"."的前缀）
-        suffix = prefix.split(".", 1)[1]
 
-        # 创建新目录
-        new_dir = os.path.join("cpp", f"{num}.{suffix}")
+def delete_icloud_files(dir_path="."):
+    # 遍历指定目录和其所有子目录
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            # 如果文件以".icloud"结尾，删除它
+            if file.endswith(".icloud"):
+                file_path = os.path.join(root, file)
+                os.remove(file_path)
+                print(f"Deleted file: {file_path}")
 
-        # 如果新目录已存在，删除源文件
-        if os.path.exists(new_dir):
-            os.remove(filename)
-        else:
-            # 否则，移动并重命名文件
-            os.makedirs(new_dir, exist_ok=True)
-            shutil.move(filename, os.path.join(new_dir, "solution.cpp"))
+
+# 调用函数删除当前目录及其所有子目录下的".icloud"文件
+delete_icloud_files(".")

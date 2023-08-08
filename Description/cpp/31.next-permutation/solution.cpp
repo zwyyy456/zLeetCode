91f2ce8c9b5b6c288da2c7554a0ce08f461736a8
zwyyy456 <zwyyy456@hotmail.com>
2023-08-08 13:30:18 +0800

    win11, local

---
+++ Description/cpp/31.next-permutation/solution.cpp
@@ -0,0 +1,55 @@
+// Created by zwyyy456 at 2023/08/08 09:55
+// leetgo: 1.3.5
+// https://leetcode.cn/problems/next-permutation/
+
+#include <algorithm>
+#include <bits/stdc++.h>
+#include "LC_IO.h"
+using namespace std;
+
+// @lc code=begin
+
+class Solution {
+  public:
+    void nextPermutation(vector<int> &nums) {
+        int n = nums.size();
+        int resr = 0, resl = -1;
+        int maxnum = nums[0], second = nums[0];
+        stack<int> stk;
+        for (int i = n - 1; i >= 0; --i) {
+            while (!stk.empty() && nums[i] < nums[stk.top()]) {
+                resr = stk.top();
+                resl = i;
+                stk.pop();
+            }
+            if (resr > 0) {
+                break;
+            }
+            stk.push(i);
+        }
+        if (resr <= 0) {
+            sort(nums.begin(), nums.end());
+        } else {
+            swap(nums[resl], nums[resr]);
+            sort(nums.begin() + resl + 1, nums.end());
+        }
+    }
+};
+
+// @lc code=end
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    stringstream out_stream;
+
+    vector<int> nums;
+    LeetCodeIO::scan(cin, nums);
+
+    Solution *obj = new Solution();
+    auto res = obj->nextPermutation(nums);
+    LeetCodeIO::print(out_stream, res);
+    cout << "\noutput: " << out_stream.rdbuf() << endl;
+
+    delete obj;
+    return 0;
+}

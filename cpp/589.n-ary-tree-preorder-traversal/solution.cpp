cd4679b8f2dcee30318f8e242b562f86b751397c
zwyyy456 <zwyyy456@hotmail.com>
2024-02-18 14:40:09 +0800

    leetcode

--- cpp/589.n-ary-tree-preorder-traversal/solution.cpp
+++ cpp/589.n-ary-tree-preorder-traversal/solution.cpp
@@ -1,10 +1,13 @@
-/*
- * @lc app=leetcode id=589 lang=cpp
- *
- * [589] N-ary Tree Preorder Traversal
- */
+// Created by zwyyy456 at 2024/02/18 12:25
+// leetgo: 1.4.1
+// https://leetcode.com/problems/n-ary-tree-preorder-traversal/
 
-// @lc code=start
+#include <bits/stdc++.h>
+#include "LC_IO.h"
+using namespace std;
+
+// @lc code=begin
+
 /*
 // Definition for a Node.
 class Node {
@@ -24,19 +27,41 @@ public:
     }
 };
 */
-#include <vector>
-using std::vector;
+
 class Solution {
   public:
-    vector<int> res;
-    vector<int> preorder(Node *root) {
-        if (root == nullptr)
-            return res;
+    void solve(Node *root, vector<int> &res) {
         res.push_back(root->val);
-        for (int i = 0; i < root->children.size(); i++) {
-            preorder(root->children[i]);
+        for (auto *ptr : root->children) {
+            solve(ptr, res);
         }
         return res;
     }
+    vector<int> preorder(Node *root) {
+        vector<int> res;
+        if (root == nullptr) {
+            return res;
+        }
+        solve(root, res);
+        return res;
+    }
 };
+
 // @lc code=end
+
+// Warning: this is a manual question, the generated test code may be incorrect.
+int main() {
+    ios_base::sync_with_stdio(false);
+    stringstream out_stream;
+
+    int root;
+    LeetCodeIO::scan(cin, root);
+
+    Solution *obj = new Solution();
+    auto res = obj->preorder(root);
+    LeetCodeIO::print(out_stream, res);
+    cout << "\noutput: " << out_stream.rdbuf() << endl;
+
+    delete obj;
+    return 0;
+}
